5175c3d410d27e960cda8a2eb7eb9bdfe37db9bd
Boots <hello@boots.media>
2025-12-23 16:03:27 -0800

    rename textimage to textmedia

--- src/components/TextImage/TextMedia.tsx
+++ src/components/TextImage/TextMedia.tsx
@@ -2,43 +2,40 @@ import React from 'react'
 import Image from 'next/image'
 import clsx from 'clsx'
 
-import styles from './TextImage.module.css'
+import styles from './TextMedia.module.css'
 
-interface TextImageProps {
-  heading?: string
-  body?: React.ReactNode
-  imageSrc: string
+interface TextMediaProps {
+  imageSrc?: string
   imageAlt: string
   imageWidth?: number
   imageHeight?: number
+	videoSrc?: string
   reverse?: boolean
   className?: string
   children?: React.ReactNode
 }
 
-const TextImage = ({
-  heading,
-  body,
+const TextMedia = ({
   children,
   imageSrc,
   imageAlt,
   imageWidth = 640,
   imageHeight = 480,
+	videoSrc,
   reverse = false,
   className = '',
-}: TextImageProps) => {
+}: TextMediaProps) => {
   return (
     <section className={clsx(styles.wrapper, reverse && styles.reverse, className)}>
       <div className={styles.text}>
-        {heading ? <h3>{heading}</h3> : null}
-        {body ? <div className={styles.body}>{body}</div> : null}
         {children}
       </div>
       <div className={styles.image}>
-        <Image src={imageSrc} alt={imageAlt} width={imageWidth} height={imageHeight} />
+        {imageSrc && <Image src={imageSrc} alt={imageAlt} width={imageWidth} height={imageHeight} />}
+        {videoSrc && <video src={videoSrc} width={imageWidth} height={imageHeight} controls />}
       </div>
     </section>
   )
 }
 
-export default TextImage
+export default TextMedia
--- src/components/TextImage/index.tsx
+++ src/components/TextImage/index.tsx
@@ -1,1 +1,1 @@
-export { default } from './TextImage'
+export { default } from './TextMedia'
