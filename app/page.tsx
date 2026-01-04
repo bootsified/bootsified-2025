fd9dbc302efb5cfb2466d1c3fcb03ae41a36e83a
Boots <hello@boots.media>
2026-01-04 03:04:47 -0800

    update homepage metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-import { SITE_PUBLIC_URL, SEO_DEFAULT_TITLE, SEO_DEFAULT_DESCRIPTION } from '@/utils/constants'
+import { SITE_PUBLIC_URL, SEO_DEFAULT_TITLE, SEO_DEFAULT_DESCRIPTION, SEO_DEFAULT_IMAGE } from '@/utils/constants'
 import Schema from '@/components/Schema'
 import Definition from '@/components/Definition'
 import Intro from '@/components/Intro'
 
 export const metadata = {
-  title: SEO_DEFAULT_TITLE,
-  description: SEO_DEFAULT_DESCRIPTION,
-  alternates: {
-    canonical: SITE_PUBLIC_URL
-  }
+	title: SEO_DEFAULT_TITLE,
+	description: SEO_DEFAULT_DESCRIPTION,
+	alternates: {
+		canonical: SITE_PUBLIC_URL
+	},
+	twitter: {
+		title: SEO_DEFAULT_TITLE,
+		description: SEO_DEFAULT_DESCRIPTION,
+		images: [SEO_DEFAULT_IMAGE], // Must be an absolute URL
+	},
+	openGraph: {
+		title: SEO_DEFAULT_TITLE,
+		description: SEO_DEFAULT_DESCRIPTION,
+		url: SITE_PUBLIC_URL,
+		images: [
+			{
+				url: SEO_DEFAULT_IMAGE, // Must be an absolute URL
+				width: 1200,
+				height: 630,
+			},
+		],
+	},
 }
 
 const HomePage = () => {
