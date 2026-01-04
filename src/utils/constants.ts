11c8c039cfe80431904760d8e58a82cc229117fe
Boots <hello@boots.media>
2026-01-04 02:25:27 -0800

    update url constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,7 +8,7 @@ export const SITE_NAME = 'John "Boots" Highland'
 export const SITE_NAME_SHORT = 'Boots!!!'
 
 export const SITE_PUBLIC_DOMAIN = 'boots.dev'
-export const SITE_PUBLIC_URL = `https://${SITE_PUBLIC_DOMAIN}`
+export const SITE_PUBLIC_URL = process.env.NEXT_PUBLIC_SITE_URL || `https://${SITE_PUBLIC_DOMAIN}`
 
 export const SEO_DEFAULT_TITLE = `Front-End Developer & Musician | ${SITE_NAME} Portfolio`
 export const SEO_DEFAULT_DESCRIPTION = 'Front-end developer and lifelong musician building accessible, responsive web interfaces with care, rhythm, and attention to detail.'
