2aee5b0e296b8d2a2f502961a8298e512b877edb
Boots <hello@boots.media>
2026-01-19 00:18:37 -0800

    Updates 404 page with new design
    Replaces the generic 404 error page with a more engaging and visually appealing design.
    
    Uses an image of Sasquatch holding a 404 sign to indicate the page is not found.
    The new layout improves the user experience.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
 import styles from './not-found.module.css'
 import Button from '@/components/Button'
 import { PageHandleSetter } from '@/components/PageHandleSetter'
+import Image from 'next/image'
 
 export const metadata = {
   title: '404: Page Not Found',
@@ -11,16 +12,16 @@ export default function NotFound() {
   return (
     <div className={styles.container}>
       <PageHandleSetter handle="other" />
+      <h1 className="srOnly">404: Page Not Found</h1>
       <div className={styles.content}>
-        <h1 className={styles.title}>
-          <span className={styles.errorCode}>404</span>
-        </h1>
-        <p className={styles.message}>
-          Oops! The page you&rsquo;re looking for doesn&rsquo;t exist.
-        </p>
-        <Button href="/" className={styles.homeLink}>
-          Return Home
-        </Button>
+        <div className={styles.sasquatch}>
+					<Image src="/images/sasquatch-404-sign.png" alt="Sasquatch" width={209} height={400} />
+				</div>
+				<div className={styles.text}>
+					<p>Uhhh...<br />
+					Are you lost or somethin&rsquo;?</p>
+					<Button href="/" className={styles.homeLink}>Go home</Button>
+				</div>
       </div>
     </div>
   )
