ae04b923690724bf3dbe9e0e84e6a3f9b7e8e4d1
Boots <hello@boots.media>
2026-02-07 22:13:59 -0800

    tweak to blog landing heading

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -51,7 +51,7 @@ const BlogPage = async () => {
 			<h1 className='srOnly'>Blog</h1>
 			<TextBlock padBottom='3em' padTop='1em' narrow>
         <h2 className='h1'>Stuffs & Thangs</h2>
-        <p>News, stories, thoughts, silliness, <s>recipes</s>&hellip; It won&rsquo;t be very often, but when I finally do have something to say, this is where you&rsquo;ll find it.</p>
+        <p>News, stories, thoughts, silliness, <s>recipes</s>&hellip; It probably won&rsquo;t be very often, but when I finally do have something to say, this is where you&rsquo;ll find it.</p>
 			</TextBlock>
       <BlogList posts={posts} />
     </div>
